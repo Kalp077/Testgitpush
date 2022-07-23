00a039f0ea5e3e83c9c4259d9123fbab1459471f
HP <kshevwale077@gmail.com>
2022-07-23 13:18:02 +0530

    This is my first code for pushing the gipush

---
+++ Test1.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://<username>:<Kshevale0077>@cluster0.mib8i.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+
+# d = {
+#     "name" : "kalpesh",
+#     "email" : "kalpesh.shevale@",
+#     "surname" : "shevale"}
+#
+# db1 = client['mongotest']
+# coll = db1['test1']
+# coll.insert_one(d )
